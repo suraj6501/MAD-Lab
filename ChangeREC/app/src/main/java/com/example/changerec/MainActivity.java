a955813f9714836b05c01005bdeb8650c0a09784
suraj6501 <surajn6501@gmail.com>
2022-03-12 19:11:44 +0530

    MAD Lab Assignment commit

--- ChangeREC/app/src/main/java/com/example/changerec/MainActivity.java
+++ ChangeREC/app/src/main/java/com/example/changerec/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.changerec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     Button b1, b2, b3;
     int count = 10;
+    LinearLayout lay;
+    int switchVar = 1;
+    String color = "WHITE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +26,63 @@ public class MainActivity extends AppCompatActivity {
         b1 = (Button) findViewById(R.id.bFontSize);
         b2 = (Button) findViewById(R.id.bFontColor);
         b3 = (Button) findViewById(R.id.bBgColor);
+        lay = (LinearLayout) findViewById(R.id.linearLayout);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (count <= 50) {
                     count = count + 5;
-                    tv.setText("" + count);
+                    tv.setTextSize(count);
                 } else {
                     count = 10;
-                    tv.setText("" + count);
+                    tv.setTextSize(count);
                 }
             }
         });
+
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(tv.getCurrentTextColor() == Color.BLUE){
+                    tv.setTextColor(Color.BLACK);
+                }
+                else if (tv.getCurrentTextColor() == Color.BLACK){
+                    tv.setTextColor(Color.RED);
+                }
+                else if(tv.getCurrentTextColor() == Color.RED){
+                    tv.setTextColor(Color.GREEN);
+                }
+                else if(tv.getCurrentTextColor() == Color.GREEN){
+                    tv.setTextColor(Color.BLUE);
+                }
             }
         });
+
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                switch(switchVar) {
 
+                    case 1:
+                        lay.setBackgroundColor(Color.RED);
+                        switchVar = switchVar + 1;
+                        break;
+
+                    case 2:
+                        lay.setBackgroundColor(Color.GREEN);
+                        switchVar = switchVar + 1;
+                        break;
+
+                    case 3:
+                        lay.setBackgroundColor(Color.BLUE);
+                        switchVar = switchVar + 1;
+                        break;
+
+                    default:
+                        switchVar = 1;
+                        lay.setBackgroundColor(Color.WHITE);
+                }
             }
         });
     }
